9a019c60750acbe38ce9827b360924945a3fd6c5
SusoDiz <jesusgonzalezdiaz77@gmail.com>
2024-05-15 10:44:23 +0100

    Agregar archivos de proyecto.

---
+++ Proyecto_RAZOR/Pages/Ejer1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_RAZOR.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
