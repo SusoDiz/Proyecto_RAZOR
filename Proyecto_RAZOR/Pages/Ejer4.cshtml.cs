f57f3db2813f84124a6e4bf6499218fc2eb129ae
SusoDiz <jesusgonzalezdiaz77@gmail.com>
2024-05-28 11:41:22 +0100

    Ejercicio 3 y 4

---
+++ Proyecto_RAZOR/Pages/Ejer4.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_RAZOR.Pages
+{
+    public class Ejer4Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
